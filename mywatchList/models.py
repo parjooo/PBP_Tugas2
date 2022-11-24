264bd9bc9d4ae1e826088bd03faa3341db942209
Faris Bayhaqi <94624202+parjooo@users.noreply.github.com>
2022-11-24 10:59:46 +0700

    Update models.py

--- mywatchList/models.py
+++ mywatchList/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
-class WatchList(models.Model):
-    watched = models.BooleanField()
-    title = models.CharField(max_length = 100)
-    rating = models.FloatField()
-    release_date = models.CharField(max_length = 20)
+class MyWatchList(models.Model):
+    watched = models.CharField(max_length=3)
+    title = models.TextField()
+    rating = models.IntegerField()
+    release_date = models.TextField()
     review = models.TextField()
